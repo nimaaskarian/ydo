67e64b9b3b532c072fc45c6527671086d15a5d3b
nimaaskarian <nimaaskarian.work@outlook.com>
2025-03-13 17:50:10 +0330

    Update edit command handling dependencies

--- cmd/edit.go
+++ cmd/edit.go
@@ -4,31 +4,36 @@ import (
 	"log/slog"
 	"strings"
 
-	"github.com/nimaaskarian/ydo/core"
 	"github.com/spf13/cobra"
 )
 
+var no_deps bool
 func init() {
   rootCmd.AddCommand(editCmd)
-  editCmd.Flags().StringArrayVarP(&deps, "deps", "d", []string{}, "dependencies for the task to add")
-  editCmd.RegisterFlagCompletionFunc("deps", TaskKeyCompletion)
+  editCmd.Flags().StringArrayVarP(&deps, "add-deps", "d", []string{}, "add dependencies for the task")
+  editCmd.Flags().BoolVarP(&no_deps, "no-deps", "D", false, "delete dependencies for a task (if used with dependencies, replaces the deps with new deps)")
+  editCmd.RegisterFlagCompletionFunc("add-deps", TaskKeyCompletion)
   editCmd.ValidArgsFunction = TaskKeyCompletionOnFirst
 }
 
 var editCmd = &cobra.Command{
-  Use: "edit [key] [new task]",
+  Use: "edit [key] [new task message (optional)]",
   Short: "edit a task",
   Run: func(cmd *cobra.Command, args []string) {
     key := args[0]
-    task := strings.Join(args[1:], " ")
-    if task == "" {
-      task = taskmap[key].Task
+    task := taskmap[key]
+    if new_task := strings.Join(args[1:], " "); new_task != "" {
+      task.Task = new_task
     }
     taskmap.MustHaveTask(key)
     for _,key := range deps {
       taskmap.MustHaveTask(key)
     }
-    taskmap[key] = core.Task {Task: task, Deps: deps}
+    if no_deps {
+      task.Deps = make([]string, 0, len(deps))
+    }
+    task.Deps = append(task.Deps, deps...)
+    taskmap[key] = task
     slog.Info("Task edited", "task", taskmap[key])
     taskmap.Write(tasks_path)
   },
