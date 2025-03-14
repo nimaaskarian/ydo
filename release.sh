b7f7e08a1c15736d1e3b0d91ff4b7a9112d1215e
nimaaskarian <nimaaskarian.work@outlook.com>
2025-03-14 23:32:09 +0330

    Add release.sh

---
+++ release.sh
@@ -0,0 +1,4 @@
+last_tag=$(git tag | tail -n 1)
+git tag "$1"
+make && \
+gh release create "$1" ydo ydo.exe  --title "$1" --notes "**Full Changelog**: https://github.com/nimaaskarian/ydo/compare/$last_tag...$1" --repo nimaaskarian/ydo
